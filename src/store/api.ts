bcd8c6abb50b0bc1d664b20bc7befad098b253cf
AJ <aj.fernando@holoplot.com>
2020-01-08 18:42:52 +0100

    Api function loginUser() POST conduitApi & collect response

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,13 +1,22 @@
+import Vue from "vue";
 import axios from "axios";
 
+import { UserSubmit, UserResponse, User } from "./models";
+
 export const conduitApi = axios.create({
-  baseURL: "https://conduit.productionready.io/api"
+  baseURL: 'https://conduit.productionready.io/api'
 });
 
 export function setJWT(jwt: string) {
-  conduitApi.defaults.headers.common["Authorization"] = `Token ${jwt}`;
+  conduitApi.defaults.headers.common['Authorization'] = `Token ${jwt}`;
 }
 
 export function clearJWT() {
-  delete conduitApi.defaults.headers.common["Authorization"];
+  delete conduitApi.defaults.headers.common['Authorization'];
+}
+
+export async function loginUser(userSubmit: UserSubmit): Promise<User> {
+  console.log(`api.ts : ${userSubmit.email} | ${userSubmit.password}`);
+  const response = await conduitApi.post('/users/login', userSubmit)
+  return (response.data as UserResponse).user;
 }
